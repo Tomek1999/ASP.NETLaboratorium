9d9cf7ab125eb7e5bcc9af11087bfdd1324117cf
Tomasz.Wrona <Tomasz.Wrona@PK9-07>
2022-10-15 11:14:10 +0200

    Dodaj pliki projektów.

---
+++ FirstMVCApp/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using FirstMVCApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstMVCApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public string Hello()
+        {
+            string name = Request.Query["name"];
+            string age = Request.Query["age"];
+            Response.StatusCode = 200;
+            return $"Hello { name}, age {age}";
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
